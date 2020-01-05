66eb1ee2b76d29cea657125f57ac0ee9ce0c3b93
堀知海 <kazuumi.hori@spacemarket.co.jp>
2020-01-05 10:13:34 +0900

    Initial commit

---
+++ lib/redux/modules/counter.dart
@@ -0,0 +1,19 @@
+import 'package:redux/redux.dart';
+
+class CounterState {
+  final int counter;
+
+  CounterState({this.counter});
+}
+
+class IncrementAction {
+
+}
+
+CounterState counterReducer(CounterState state, IncrementAction action) {
+  return CounterState(counter: state.counter + 1);
+}
+
+final reducers = combineReducers<CounterState>([
+  TypedReducer<CounterState, IncrementAction>(counterReducer)
+]);
